fe85023e7c545ab8aa342c48410967a1401d38f3
Артем <artemgolyshkov@mail.ru>
2023-09-03 12:42:45 +0300

    HW 4.1

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
     List<Faculty> findAllByColorIgnoreCase(String color);
     List<Faculty> findAllByNameIgnoreCase(String name);
+
+    Object findByStudents_Id(Long any);
 }
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findAllByAge(Integer age);
     List<Student> findByAgeBetween(int min, int max);
+
+    Object findAllByFaculty_Id(Long any);
 }
